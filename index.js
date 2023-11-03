ec5ebbf2fbb2f7f222f5c4e97b0c96761f1e4947
Tiffany Man <tiffanyman19@gmail.com>
2023-11-03 15:49:42 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event) 
+{
+   return  names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`)
+}
+
+console.log(writeCards(names, "surpise"))
+
+function countDown(number) 
+{
+    while (number > 0)
+    {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+ }
